1d06c93ce49f1f3fcd10861eaed3f9b882b2f4d7
Ikechi Ucheagwu <ikechi@hotmail.com>
2022-09-28 21:10:04 +0100

    Question 12 alternative method added.

--- src/main/java/org/example/_12_first_missing_positive/FirstMissingPositive.java
+++ src/main/java/org/example/_12_first_missing_positive/FirstMissingPositive.java
@@ -6,10 +6,10 @@ public class FirstMissingPositive {
 
     public static void main(String[] args) {
         System.out.println(firstMissingPositive(new int[]{3,4,-1,1}));
-        System.out.println(firstMissingPositive(new int[]{1,2,0}));
-        System.out.println(firstMissingPositive(new int[]{1}));
-        System.out.println(firstMissingPositive(new int[]{7, 3, 5, 7, 9}));
-        System.out.println(firstMissingPositive(new int[]{0}));
+        System.out.println(firstMissingPositive1(new int[]{1,2,0}));
+        System.out.println(firstMissingPositive1(new int[]{1}));
+        System.out.println(firstMissingPositive1(new int[]{7, 3, 5, 7, 9}));
+        System.out.println(firstMissingPositive1(new int[]{0}));
     }
 
     public static int firstMissingPositive(int[] nums) {
@@ -23,5 +23,18 @@ public class FirstMissingPositive {
         }
 
         return change + 1;
+    }
+
+    public static int firstMissingPositive1(int[] nums) {
+        Arrays.sort(nums);
+        int value = 1;
+
+        for (int elemI : nums) {
+            if (elemI == value) {
+                value++;
+            }
+        }
+
+        return value;
     }
 }
